1351f940061847f7f8e44aaf2bdcbcdfe40fa2e9
Randolph Pe Benito <randolph@xy-randolph.xyber.ph>
2016-05-02 14:32:27 +0800

    Configured ALLOWED_HOSTS and database.

--- myproject/settings.py
+++ myproject/settings.py
@@ -84,6 +84,14 @@ DATABASES = {
 }
 
 
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
+
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+ALLOWED_HOSTS = ['*']
+
+
 # Internationalization
 # https://docs.djangoproject.com/en/1.8/topics/i18n/
 
